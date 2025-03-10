1920a1b11f0c7fceb52f2ae4a1e9dc827274731e
Himanshu Jain <himanshu_jain@epam.com>
2025-03-10 16:17:53 +0530

    Access token Refresh token and instance methods pre hook in mongoose

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
 # backend development with javascript
 
 Starting with mongoose for data modelling (enums, creating small schema models in single model as well so that it can be used as array in another schema model)
+
+CORS(cross origin resource sharing) - It allows us to make cross origin requests means if frontend is hosted at localhost:3000 and making a request
+to a backend which is hosted at localhost:5000 then the request will not happen successfully if cors is not configured.
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,4 +47,45 @@ const userSchema = new mongoose.Schema({
 
 }, {timeStamps: true})
 
+// Password encryption: 
+// 1. jaise hi user n first time password daala ya existing pswd ko update kiya tabhi apan password ko save krayenge to if condition m vhi handle
+//    kr rhe h i.e. tabhi pswd ko save krvayenge varna aage badh jaayenge
+// 2. pswd encryptn is time taking to async ka use krenge and arrow functn use nhi kiya callback m kyu ki they doesn't have context i.e. this
+//    keyword use nhi kr skte unme
+
+userSchema.pre("save", async function (next) {
+
+    if(!this.isModified("password"))  return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
+
+// this is the way to add instance methods to schema in mongoose. so isPasswordSame is an instance method which is added to userSchema now.
+userSchema.methods.isPasswordSame = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
 export const User = mongoose.model("User", userSchema)
